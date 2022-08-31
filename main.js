a2bc788c9bb74cd128144579ce62608545829232
alijonovshohrux858@gmail.com <alijonovshohrux858@gmail.com>
2022-08-31 15:31:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+ let a = Math.random()*10;
+let b = Math.random()*10;
+    
+    a=parseInt(a);
+    b=parseInt(b);
+
+console.log("Birinchi sonimiz A = " + a);
+console.log("Ikkinchi sonimiz B = " + b);
+
+
+if(a>5,b>5){
+    c=a-5;
+    d=b-5;
+    if(c<d){
+        console.log(a + 'soni 5 ga eng yaqin')
+    }
+    if(c===d){
+        console.log('ozaro teng sonlar')
+    }
+    else(
+        console.log(b + ' soni 5 ga eng yaqini')
+    )
+}
+// let a = Math.random()* 100;
+// a = parseInt(a);
+
+// if(a % 15 === 0){
+//     console.log(a + " soni 3 va 5 ga bo'linadi");
+// }else if(a % 5 ===0){
+//     console.log(a + " soni 5 ga bo'linadi");
+// }else if(a % 3 === 0){
+//     console.log(a + " soni 3 ga bolinadi");
+// }else{
+//     console.log(a + " soni 3 va 5 ga bo'inmaydi");
+// }
+
